cb56d9a8d41bc5d1c00db3fc56d20947f45e6653
Nikita <generalhammond16@gmail.com>
2021-09-06 17:09:33 +0300

    miniprintf: add min width parameter
    Signed-off-by: Nikita <generalhammond16@gmail.com>

--- miniPrintf/main.c
+++ miniPrintf/main.c
@@ -1,54 +1,135 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAXWORD 100
 
 void miniprintf(char *fmt, ...);
+int readnum(char **p);
 
 int main()
 {
-    miniprintf("%s decimal: %d float: %f octal: %o\n", "This is", 1234, 1234.345, 54321);
+    miniprintf("%11s decimal: %10d float: %15f hex: %20x\n", "This is", 1234, 1234.345, 54321);
     return 0;
 }
+
+struct {
+        int minus;
+        int min_width;
+        int precision;
+} opt;
+
+const char control[] = {
+        'd', 'i',
+        'o',
+        'x', 'X',
+        'u',
+        'c',
+        's',
+        'f'
+};
 
 void miniprintf(char *fmt, ...)
 {
         va_list ap;
-        char *p, *sval;
-        int ival;
+        char *p, *sval,
+        format[MAXWORD];
+        int ival, percent = 0;
         double dval;
 
         va_start(ap, fmt);
         for (p = fmt; *p; p++) {
-                if (*p != '%') {
-                        putchar(*p);
+                if (*p == '%') {
+                        if (percent) {
+                                percent = 0;
+                                putchar(*p);
+                        } else {
+                                if (isdigit(*(p+1)))
+                                        opt.min_width = readnum(&p);
+                                percent = 1;
+                        }
                         continue;
                 }
-                switch (*++p) {
-                case 'd':
-                case 'i':
-                        ival = va_arg(ap, int);
-                        printf("%d", ival);
-                        break;
-                case 'o':
-                        ival = va_arg(ap, int);
-                        printf("%o", ival);
-                        break;
-                case 'f':
-                        dval = va_arg(ap, double);
-                        printf("%f", dval);
-                        break;
-                case 's':
-                        for (sval = va_arg(ap, char*); *sval; sval++)
-                                putchar(*sval);
-                        break;
-                default:
+                if (percent) {
+                        switch (*p) {
+                        case 'd':
+                        case 'i':
+                                ival = va_arg(ap, int);
+                                if (opt.min_width != 0)
+                                        sprintf(format, "%%%dd", opt.min_width);
+                                else
+                                        strcpy(format, "%d");
+                                printf(format, ival);
+                                memset(&opt, 0, sizeof(opt));
+                                percent = 0;
+                                break;
+                        case 'o':
+                                ival = va_arg(ap, int);
+                                if (opt.min_width != 0)
+                                        sprintf(format, "%%%do", opt.min_width);
+                                else
+                                        strcpy(format, "%o");
+                                printf(format, ival);
+                                memset(&opt, 0, sizeof(opt));
+                                percent = 0;
+                                break;
+                        case 'x':
+                        case 'X':
+                                ival = va_arg(ap, int);
+                                if (opt.min_width != 0)
+                                        sprintf(format, "%%%dx", opt.min_width);
+                                else
+                                        strcpy(format, "%x");
+                                printf(format, ival);
+                                memset(&opt, 0, sizeof(opt));
+                                percent = 0;
+                                break;
+                        case 'f':
+                                dval = va_arg(ap, double);
+                                if (opt.min_width != 0)
+                                        sprintf(format, "%%%df", opt.min_width);
+                                else
+                                        strcpy(format, "%f");
+                                printf(format, dval);
+                                memset(&opt, 0, sizeof(opt));
+                                percent = 0;
+                                break;
+                        case 's':
+                                int printed = 0;
+                                for (sval = va_arg(ap, char*); *sval; sval++, printed++)
+                                        putchar(*sval);
+                                for ( ; printed < opt.min_width; printed++)
+                                        putchar(' ');
+                                memset(&opt, 0, sizeof(opt));
+                                percent = 0;
+                                break;
+                        case '-':
+                                if (opt.minus)
+                                opt.minus = 1;
+                        default:
+                                putchar(*p);
+                                break;
+                        }
+                } else
                         putchar(*p);
-                        break;
-                }
         }
         va_end(ap);
 }
 
+int readnum(char **p)
+{
+        (*p)++;
+        char numbuf[MAXWORD];
+
+        int i = 0;
+        for ( ; **p >= 0 && isdigit(**p); (*p)++)
+                numbuf[i++] = **p;
+        (*p)--;
+        numbuf[i] = '\0';
+        return atoi(numbuf);
+}
 
 
 
