c8be8fbfa69e9fe2096db8d90e14df3e7ed3c334
Nikita <generalhammond16@gmail.com>
2021-10-21 01:34:05 +0300

    fopen: add win Makefile
    Signed-off-by: Nikita <generalhammond16@gmail.com>

---
+++ ScanfCalc/make.bat
@@ -0,0 +1,17 @@
+@echo off
+
+set FILENAME=calc
+set FILES=*.c
+
+set CFLAGS=-O0 -s -mconsole
+set LKFLAGS=-Wl,--gc-sections
+
+@echo on
+gcc %CFLAGS% %LKFLAGS% %FILES% -o %FILENAME%
+@echo off
+	if errorlevel 1 goto terminate
+@echo on
+	dir
+	:terminate
+	pause
+© 2021 GitHub, Inc.
