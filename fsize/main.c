6f8e7b5ac636c3b2906114d852cfd3380782ac60
Nikita Piatygorskiy <generalhammond16@gmail.com>
2021-11-12 23:16:41 +0200

    fsize: implemented print_file_information
    Signed-off-by: Nikita Piatygorskiy <generalhammond16@gmail.com>

--- fsize/main.c
+++ fsize/main.c
@@ -1,19 +1,98 @@
 #include <windows.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdint.h>
 
+#define MAX_NAME 100
+
+int header_already_printed = 0;
+static void printf_file_information(LPBY_HANDLE_FILE_INFORMATION finfo, HANDLE hFile)
+{
+        char szFilePath[MAX_NAME];
+        if (!header_already_printed) {
+                printf("Attr\t"
+                        "Created\t\t\t"
+                        "Size\t"
+                        "ID\t\t\t"
+                        "Name\n"
+                        "----\t"
+                        "---------------\t\t"
+                        "------\t"
+                        "-----------------\t"
+                        "-----------------\n");
+                header_already_printed = 1;
+        }
+        
+        // print attribute fileld
+        DWORD fattr = finfo->dwFileAttributes;
+        if (fattr & FILE_ATTRIBUTE_DIRECTORY)
+                printf("d");
+        else printf("f");
+        if (fattr & FILE_ATTRIBUTE_ENCRYPTED)
+                printf("e");
+        else printf("-");
+        if (fattr & FILE_ATTRIBUTE_COMPRESSED)
+                printf("a");
+        else printf("-");
+        if (fattr & FILE_ATTRIBUTE_SYSTEM)
+                printf("s\t");
+        else printf("-\t");
+        
+        // print creation time
+        FILETIME time = finfo->ftCreationTime;
+        SYSTEMTIME stime;
+        FileTimeToSystemTime(&time, &stime);
+        printf("%02d/%02d/%04d %02d:%02d\t", stime.wDay, stime.wMonth, stime.wYear,
+        stime.wHour, stime.wMinute);
+        
+        // print file size
+        uint64_t fsize = finfo->nFileSizeHigh;
+        fsize = fsize << (sizeof(DWORD) * 8);
+        fsize |= finfo->nFileSizeLow;
+        if (fsize < 1<<10)
+                printf("%llub\t", fsize);      
+        else if (fsize < 1<<20)
+                printf("%lluKB\t", fsize / (1<<10));      
+        else if (fsize < 1<<30)
+                printf("%lluMB\t", fsize / (1<<20));      
+        else
+                printf("%lluGB\t", fsize / (1<<30));      
+        
+        // print index
+        uint64_t qindex = finfo->nFileIndexHigh;
+        qindex = qindex << (sizeof(DWORD) * 8);
+        qindex |= finfo->nFileIndexLow;
+        printf("%llu\t\t", qindex);
+        
+        //print name
+        GetFinalPathNameByHandleA(hFile, &szFilePath, MAX_NAME, FILE_NAME_NORMALIZED);
+        int pos = strlen(szFilePath);        
+        while (szFilePath[--pos] != '\\' && szFilePath[pos] != ':' && pos > 0);
+        pos++;
+        char *ptrName = szFilePath + pos;
+        puts(ptrName);
+}
 
 int main(int argc, char *argv[])
 {
-        int len = strlen(argv[1]);
+        if (argc <= 1) {
+                puts("Specify at least one parameter");
+                return 1;
+        }
         
-        while (argv[1][--len] != '\\' && argv[1][len] != ':')
-                argv[1][len] = '\0';
-        
-        HANDLE hDir = CreateFile(argv[1], GENERIC_READ, FILE_SHARE_READ, 
+        HANDLE hFile = CreateFile(argv[1], GENERIC_READ, FILE_SHARE_READ, 
          NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL|FILE_FLAG_BACKUP_SEMANTICS, NULL);
-        if (hDir == NULL)
-                printf("Error opening directory %s", argv[1]);
+        if (hFile == INVALID_HANDLE_VALUE) {
+                printf("Error opening file %s", argv[1]);
+                return 1;
+        }
         
+        BY_HANDLE_FILE_INFORMATION fileinfo;
+        BOOL result =  GetFileInformationByHandle(hFile, &fileinfo);          
+        if (result == FALSE) {
+                printf("Unable to access file infotmation");
+                return 1;
+        }
         
+        printf_file_information(&fileinfo, hFile);
 }
